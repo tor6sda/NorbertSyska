c884309ebc9bd31c89ba6b8ae9cbc0dd0f63fe1b
Norbert <syskaxiii@gmail.com>
2020-03-28 12:48:03 +0100

    Zad2 full

--- jdbc/src/main/java/zad2.java
+++ jdbc/src/main/java/zad2.java
@@ -8,7 +8,7 @@ public class zad2 {
     public static void main(String[] args) {
         StringBuilder stringBuilder = new StringBuilder("? ");
         String questionMark = ",?";
-        int tablica[] = new int[]{1,3,5};
+        int[] tablica = new int[]{1,3,5};
 
         for (int i=1; i<tablica.length;i++){
             stringBuilder.append(questionMark);
@@ -16,19 +16,26 @@ public class zad2 {
         String questy = " update product "+
                 "set pro_description = CONCAT(pro_description, ? )"+
                 "where pro_id in ("+stringBuilder.toString()+")";
-
+        String query = "select * from product";
         try (PreparedStatement preStatement = DatabaseConnection.getInstance().getConnection().prepareStatement(questy))
         {
-            preStatement.setString(1,"brak");
-            for (int i =1;i<tablica.length;i++){
-                preStatement.setInt(i+1,tablica[i]);
+            preStatement.setString(1," brak");
+            for (int i =0;i<tablica.length;i++){
+                preStatement.setInt(i+2,tablica[i]);
             }
-            ResultSet resultSet = preStatement.executeQuery();
-            while (resultSet.next()){
+            int coutUpdatedRecords = preStatement.executeUpdate();
+            System.out.println("Zaktualizowano "+coutUpdatedRecords+" wierszy");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try ( PreparedStatement statement = DatabaseConnection.getInstance().getConnection().prepareStatement(query)) {
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
                 int id = resultSet.getInt("pro_id");
                 String name = resultSet.getString("pro_name");
                 String description = resultSet.getString("pro_description");
-                System.out.println(id + "  "+name+"  "+ description);
+                System.out.println(id + "  " + name + "  " + description);
             }
         } catch (SQLException e) {
             e.printStackTrace();
